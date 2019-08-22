c0ee9212776b51e7157752f928ea78becbafebe4
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-08-22 15:47:26 -0400

    finished step 8

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -5,10 +5,15 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     const initialajaxrequest = document.getElementById('run_ajax_root');
+    
     const pingpongajaxrequest = document.getElementById('run_ajax_pingpong');
     const sectionping = document.querySelector('.step_ping');
+    
     const countajaxrequest = document.getElementById('run_ajax_count');
     const sectionseven = document.querySelector('.step_seven');
+
+    const timeajaxrequest = document.getElementById('run_ajax_time');
+    const sectioneight = document.querySelector('.step_eight');
 
     initialajaxrequest.addEventListener('click', () => {
         const request = axios.get('http://intro-ajax-api.herokuapp.com/');
@@ -22,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const request = axios.get('http://intro-ajax-api.herokuapp.com/ping');
         request.then(function(response){
             console.log("_REQUEST_PING_MADE__");
-            console.log(request.data);
+            console.log(response.data);
             const appendDataElem = document.createElement('p');
             appendDataElem.innerHTML = response.data;
             sectionping.appendChild(appendDataElem);
@@ -43,12 +48,27 @@ document.addEventListener("DOMContentLoaded", function() {
         const request = axios.get('http://intro-ajax-api.herokuapp.com/count');
         request.then(function(response) {
             console.log("__REQUEST_COUNT_MADE__");
-            console.log(request.data);
+            console.log(response.data);
             const countDataElem = document.createElement('p');
             countDataElem.innerHTML = response.data;
             sectionseven.appendChild(countDataElem);
         });
+    });
+
+    timeajaxrequest.addEventListener('click', () => {
+        const request = axios.get("http://intro-ajax-api.herokuapp.com/time", { 
+            params: {timezone: 'Asia/Kolkata'}
+        });
+        request.then(function(response) {
+            console.log("__REQUEST_TIME_MADE__")
+            console.log(request.data);
+            const timeDataElem = document.createElement('p');
+            timeDataElem.innerHTML = response.data;
+            sectioneight.appendChild(timeDataElem);
+        })
     })
+
+    
     // const request = axios.get('http://intro-ajax-api.herokuapp.com/');
     // request.then(function(response) {
     //     console.log("__REQUEST MADE__");
